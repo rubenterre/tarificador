794fc6f8654d5d1dce925d0d994baff7e7a76100
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2024-02-13 19:34:31 +0100

    Actualización de aplicación

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,86 @@
+<script>
+</script>
+
+<header class="header">
+	<div class="header__logo">
+		<h1 class="header__h1">TARIFICADOR</h1>
+	</div>
+</header>
+<slot />
+
+<footer class="footer">
+	<div class="footer__div">
+		<p class="footer__p">Todos los derechos reservador - 2024 - Tarficador</p>
+        <a class="footer__a" href="http://rubenterre.com">Rubén Terré</a>
+	</div>
+</footer>
+
+<style>
+	:root {
+		--color-principal: #66a9b9;
+		--color-secundario: #172f58;
+		--color-terciario: #d9ecf0;
+		--fuente-principal: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+			Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	:global(*) {
+		margin: 0;
+		padding: 0;
+		outline: 0;
+		box-sizing: border-box;
+	}
+
+	:global(body) {
+		background: white;
+		color: var(--color-secundario);
+		-webkit-font-smoothing: antialiased;
+	}
+
+	:global(body, input, button) {
+		font-size: 100%;
+		font-family: var(--fuente-principal);
+	}
+
+	:global(button) {
+		cursor: pointer;
+	}
+
+	/* Header */
+
+	.header {
+		background: #172f58;
+		width: 100%;
+		height: 80px;
+	}
+
+	.header__logo {
+		padding: 30px 0px;
+	}
+
+	.header__h1 {
+		font-family: var(--fuente-principal);
+		color: white;
+		font-size: 1rem;
+		text-align: center;
+	}
+
+	/* Footer */
+
+	.footer {
+		position: fixed;
+		bottom: 0px;
+        left: 50%;
+        transform: translate(-50%,-50%);
+	}
+
+	.footer__div {
+		text-align: center;
+		font-family: var(--fuente-principal);
+		font-size: 0.8rem;
+	}
+
+    .footer__a{
+        text-decoration: none;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,102 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let horas = 0 + " h";
+	let minutos = 0 + " min";
+	let tarifa = 0 + " €/h";
+	let precioFinal = 0;
+
+	function calcularFactura(horas, minutos, tarifa) {
+		let tiempoHoras = horas * 60;
+		console.log(tiempoHoras);
+		let tiempoMinutos = parseFloat(tiempoHoras) + parseFloat(minutos);
+		console.log(tiempoMinutos);
+		let tiempoTotal = tiempoMinutos * tarifa;
+		console.log(tiempoTotal);
+		let precioFinal = tiempoTotal / 60;
+		console.log(precioFinal);
+		let tarificadorResultado = document.getElementById('tarificadorResultado');
+		tarificadorResultado.textContent = precioFinal + "€";
+	}
+</script>
+
+<section class="tarificador">
+	<div class="tarificador__tiempo">
+		<div class="tarificador__horas">
+			<input bind:value={horas} type="datetime" name="horas" id="horas"/>
+			<label for="horas">Horas trabajadas</label>
+		</div>
+		<div class="tarificador__minutos">
+			<input bind:value={minutos} type="datetime" name="minutos" id="minutos"/>
+			<label for="minutos">Minutos trabajados</label>
+		</div>
+        <div class="tarificador__tarifa">
+            <input bind:value={tarifa} type="number" name="tarifa" id="tarifa" />
+            <label for="tarifa">Tarifa por hora (€/h)</label>
+        </div>
+        <div class="tarificador__boton">
+            <button class="tarificador__button" on:click={calcularFactura(horas, minutos, tarifa)}>ENTER</button>
+        </div>
+	</div>
+	<div class="tarificador__resultado">
+		<h4 class="tarificador__h4">Precio final del trabajo</h4>
+		<h3 id="tarificadorResultado" class="tarficador__h3">
+			{precioFinal.toFixed(3)+" €"}
+		</h3>
+	</div>
+</section>
+
+<style>
+
+    .tarificador__tiempo{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .tarificador__horas{
+        display: flex;
+        flex-direction: column;
+        margin: 15px 0px;
+    }
+
+    .tarificador__minutos{
+        display: flex;
+        flex-direction: column;
+        margin: 15px 0px;
+    }
+
+    .tarificador__tarifa{
+        display: flex;
+        flex-direction: column;
+        margin: 15px 0px;
+    }
+
+    .tarificador__button{
+        background: var(--color-principal);
+        color: white;
+        font-family: var(--fuente-principal);
+        width: 230px;
+        text-align: center;
+        padding: 10px 0px;
+        border: 0px;
+        border-radius: 12px;
+        box-shadow: 0px 2px 4px 0px black;
+    }
+
+    .tarificador__button:hover{
+        background: var(--color-secundario);
+        color: white;
+        font-family: var(--fuente-principal);
+        width: 230px;
+        text-align: center;
+        padding: 10px 0px;
+        border: 0px;
+        border-radius: 12px;
+        box-shadow: 0px 2px 4px 0px black;
+    }
+
+.tarificador__resultado{
+    text-align: center;
+    margin: 30px 0px;
+}
+</style>
